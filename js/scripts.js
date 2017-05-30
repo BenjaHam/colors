fd3e701e0797076a854bdd3e4f5f6a46caaf4d2b
Benjamin Hampikian, Calla Rudolph and Max Scher <calla@>
2017-05-30 15:37:45 -0700

    add text to index.html including classes for buttons, add functions to js for buttons

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+});
+
+$(function() {
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+});
+
+$(function() {
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+});
