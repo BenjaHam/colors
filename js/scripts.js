ded959fbaf3f1d90dc3efd74898f65c0bfc9f0ef
Benjamin Hampikian, Calla Rudolph and Max Scher <max@>
2017-05-30 15:44:31 -0700

    add css styling to link with js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(function() {
   $("button#green").click(function() {
     $("body").addClass("green-background");
+  });
+
+    $("button#yellow").click(function() {
+      $("body").addClass("yellow-background");
+  });
+
+    $("button#red").click(function() {
+      $("body").addClass("red-background");
+  });
 });
 
-$(function() {
-  $("button#yellow").click(function() {
-    $("body").addClass("yellow-background");
-});
-
-$(function() {
-  $("button#red").click(function() {
-    $("body").addClass("red-background");
-});
+// $(document).ready(function() {
+//   $("button#green").click(function() {
+//     $("body").addClass("green-background");
+//   });
+//
+//   $("button#yellow").click(function() {
+//     $("body").addClass("yellow-background");
+//   });
+//
+//   $("button#red").click(function() {
+//     $("body").addClass("red-background");
+//   });
+// });
